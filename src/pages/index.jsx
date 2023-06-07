9129ecb1d9fee171d08185cba1ee5795e947edae
Yuta Okuma <y.dream.nest@icloud.com>
2023-06-07 11:13:06 +0900

    feat: :sparkles: Create AnimatedText component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,8 @@ import Layout from '@/components/Layout';
 import Head from 'next/head';
 import Image from 'next/image';
 import profilePic from '../../public/images/profile/developer-pic-1.png';
+import AnimatedText from '@/components/AnimatedText';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -16,9 +18,9 @@ export default function Home() {
             <div className='w-1/2'>
               <Image src={profilePic} alt='CodeBucks' className='w-full h-auto' />
             </div>
-            <div>
-              <h1>Turning Vision Into Reality With Code And Design. </h1>
-              <p>
+            <div className='w-1/2 flex flex-col items-center self-center'>
+              <AnimatedText text='Turning Vision Into Reality With Code And Design.' className='!text-6xl !text-left' />
+              <p className='my-4 text-base font-medium'>
                 As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications.
                 Explore my latest projects and articles, showcasing my expertise in React.js and web development.
               </p>
