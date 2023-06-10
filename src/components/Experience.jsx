44f0374a352e5a5d30f86e3eeb6dd77bee605dd6
Yuta Okuma <y.dream.nest@icloud.com>
2023-06-10 11:12:33 +0900

    feat: :sparkles: Create Experience component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,10 +1,13 @@
 import { motion, useScroll } from 'framer-motion';
 import { useRef } from 'react';
+import LiIcon from './LiIcon';
 
 const Details = ({ position, company, companyLink, time, address, work }) => {
+  const ref = useRef(null);
   return (
-    <li className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
-      <div>
+    <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
+      <LiIcon reference={ref} />
+      <motion.div initial={{ y: 50 }} whileInView={{ y: 0 }} transition={{ duration: 0.5, type: 'spring' }}>
         <h3 className='capitalize font-bold text-2xl'>
           {position}&nbsp;
           <a href={companyLink} target='_blank' className='text-primary capitalize'>
@@ -15,7 +18,7 @@ const Details = ({ position, company, companyLink, time, address, work }) => {
           {time} | {address}
         </span>
         <p className='font-medium w-full'>{work}</p>
-      </div>
+      </motion.div>
     </li>
   );
 };
@@ -33,7 +36,7 @@ const Experience = () => {
       <div ref={ref} className='w-[75%] mx-auto relative'>
         <motion.div
           style={{ scaleY: scrollYProgress }}
-          className='absolute top-0 left-8 w-[4px] h-full bg-dark origin-top'
+          className='absolute top-0 left-9 w-[4px] h-full bg-dark origin-top'
         />
         <ul className='w-full flex flex-col items-center justify-between ml-4'>
           <Details
