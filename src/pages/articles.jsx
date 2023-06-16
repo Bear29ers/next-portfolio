8be5cd4a87c561eab1ae0665fe4417fc0b5b799e
Yuta Okuma <y.dream.nest@icloud.com>
2023-06-16 17:32:59 +0900

    feat: :sparkles: Complete articles page

--- src/pages/articles.jsx
+++ src/pages/articles.jsx
@@ -35,6 +35,8 @@ const MovingImg = ({ title, img, link }) => {
       <h2 className='capitalize text-xl font-semibold hover:underline'>{title}</h2>
       <FramerImage
         style={{ x: x, y: y }}
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1, duration: 0.2 }}
         ref={imgRef}
         src={img}
         alt={title}
@@ -46,10 +48,15 @@ const MovingImg = ({ title, img, link }) => {
 
 const Article = ({ img, title, date, link }) => {
   return (
-    <li className='relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark border-r-4 border-b-4'>
+    <motion.li
+      initial={{ y: 200 }}
+      whileInView={{ y: 0, transition: { duration: 0.5, ease: 'easeInOut' } }}
+      viewport={{ once: true }}
+      className='relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark border-r-4 border-b-4'
+    >
       <MovingImg title={title} img={img} link={link} />
       <span className='text-primary font-semibold pl-4'>{date}</span>
-    </li>
+    </motion.li>
   );
 };
 
