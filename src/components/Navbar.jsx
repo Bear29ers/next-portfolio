8880e198127cab38badc096c581ca57f298c1687
Yuta Okuma <y.dream.nest@icloud.com>
2023-06-17 10:48:50 +0900

    wip: :construction: Apply dark theme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import Link from 'next/link';
 import Logo from './Logo';
 import { useRouter } from 'next/router';
-import { DribbbleIcon, GithubIcon, LinkedInIcon, PinterestIcon, TwitterIcon } from './Icon';
+import { DribbbleIcon, GithubIcon, LinkedInIcon, MoonIcon, PinterestIcon, SunIcon, TwitterIcon } from './Icon';
 import { motion } from 'framer-motion';
+import useThemeSwitcher from './hooks/useThemeSwitcher';
 
 const CustomLink = ({ href, title, className = '' }) => {
   const router = useRouter();
@@ -12,7 +13,7 @@ const CustomLink = ({ href, title, className = '' }) => {
     <Link href={href} className={`${className} relative group`}>
       {title}
       <span
-        className={`h-[2px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300
+        className={`h-[2px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 dark:bg-light
 ${router.asPath === href ? 'w-full' : 'w-0'}`}
       >
         &nbsp;
@@ -22,8 +23,10 @@ ${router.asPath === href ? 'w-full' : 'w-0'}`}
 };
 
 const NavBar = () => {
+  const [mode, setMode] = useThemeSwitcher();
+
   return (
-    <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
+    <header className='w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light'>
       <nav>
         <CustomLink href='/' title='Home' className='mr-4' />
         <CustomLink href='/about' title='About' className='mx-4' />
@@ -76,6 +79,14 @@ const NavBar = () => {
         >
           <DribbbleIcon />
         </motion.a>
+        <button
+          onClick={() => setMode(mode === 'light' ? 'dark' : 'light')}
+          className={`ml-3 flex items-center justify-center rounded-full p-1
+          ${mode === 'light' ? 'bg-dark text-light' : 'bg-light text-dark'}
+          `}
+        >
+          {mode === 'dark' ? <SunIcon className={'fill-dark'} /> : <MoonIcon className={'fill-dark'} />}
+        </button>
       </nav>
       <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
         <Logo />
--- src/components/hooks/useThemeSwitcher.js
+++ src/components/hooks/useThemeSwitcher.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const useThemeSwitcher = () => {
   const preferDarkQuery = '(prefer-color-scheme: dark)';
