d31628beabdfa1efb37e12adea0e314de60cd91c
Yuta Okuma <y.dream.nest@icloud.com>
2023-06-18 10:39:57 +0900

    feat: :sparkles: Complete applying dark theme

--- src/pages/articles.jsx
+++ src/pages/articles.jsx
@@ -52,17 +52,17 @@ const Article = ({ img, title, date, link }) => {
       initial={{ y: 200 }}
       whileInView={{ y: 0, transition: { duration: 0.5, ease: 'easeInOut' } }}
       viewport={{ once: true }}
-      className='relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark border-r-4 border-b-4'
+      className='relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark border-r-4 border-b-4 dark:bg-dark dark:border-light dark:text-light'
     >
       <MovingImg title={title} img={img} link={link} />
-      <span className='text-primary font-semibold pl-4'>{date}</span>
+      <span className='text-primary font-semibold pl-4 dark:text-primaryDark'>{date}</span>
     </motion.li>
   );
 };
 
 const FeaturedArticle = ({ img, title, time, summary, link }) => {
   return (
-    <li className='relative col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl'>
+    <li className='relative col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl dark:bg-dark dark:border-light'>
       <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl' />
       <Link href={link} target='_blank' className='inline-block w-full cursor-pointer overflow-hidden rounded-lg'>
         <FramerImage
@@ -77,7 +77,7 @@ const FeaturedArticle = ({ img, title, time, summary, link }) => {
         <h2 className='capitalize text-2xl font-bold mt-4 mb-2 hover:underline'>{title}</h2>
       </Link>
       <p className='text-sm mb-2'>{summary}</p>
-      <span className='text-primary font-semibold'>{time}</span>
+      <span className='text-primary font-semibold dark:text-primaryDark'>{time}</span>
     </li>
   );
 };
@@ -89,7 +89,7 @@ const articles = () => {
         <title>CodeBucks | About Page</title>
         <meta name='description' content='any description' />
       </Head>
-      <main className='w-full mb-16 flex flex-col items-center justify-center overflow-hidden'>
+      <main className='w-full mb-16 flex flex-col items-center justify-center overflow-hidden dark:text-light'>
         <Layout className='pt-16'>
           <AnimatedText text='Words Can Change The World!' className='mb-16' />
           <ul className='grid grid-cols-2 gap-16'>
